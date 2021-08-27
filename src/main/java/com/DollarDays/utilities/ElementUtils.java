d41fe176ec698c47050a2b4099438cd4e6823732
Madhunag <kondoju.madhuri@gmail.com>
2021-08-27 15:48:16 -0700

    AddressBookTestCases

--- src/main/java/com/DollarDays/utilities/ElementUtils.java
+++ src/main/java/com/DollarDays/utilities/ElementUtils.java
@@ -2,11 +2,13 @@ package com.DollarDays.utilities;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ElementUtils
 {
@@ -26,12 +28,12 @@ public class ElementUtils
 		try {
 			wb.isDisplayed();
 			displayed=true;
-		}
+		    }
 		catch(Exception  e)
 		{		
 			//logger.warn(e.getMessage());
-			System.out.println(e.getClass().getSimpleName() + "  " + wb.toString() +   " This WebElement is not present/displayed!!! "  );
-			logger.warn(e.getClass().getSimpleName() + "  " + wb.toString() +   " This WebElement is not present/displayed!!!");
+			System.out.println(e.getClass().getSimpleName() + "  " + wb.toString() +   " This WebElement is not present/displayed. "  );
+			logger.warn(e.getClass().getSimpleName() + "  " + wb.toString() +   " This WebElement is not present/displayed. ");
 			displayed=false;
 		}
 		return displayed;		
@@ -68,7 +70,7 @@ public class ElementUtils
 		}
 	}
 	
-	//To do click on a webelement by using sendKeys(Keys.RETURN)
+	//To do click on a webelement
 	public void performElementClickReturn(WebElement wb)
 	{
 		if(isElementClickable(wb))
@@ -185,4 +187,83 @@ public class ElementUtils
 			return null;
 		 }    	
     }
-}
+    
+    //To check if the webelement is a dropdown or not
+    public boolean isElementDropDown(WebElement wb)
+    {
+  	  boolean isElementDropDown=false;
+    
+  	  if(isElementDisplayed(wb))
+  	  {
+  		isElementDropDown=wb.getTagName().equals("select");
+  	  }
+  	  return isElementDropDown;
+
+  	 }
+    
+    //To select the dropdown value using visibleText method
+    public Select getDropdownValueByVisibleText(WebElement wb,String str)
+    {
+        performElementClick(wb);
+    	Select dropdownvalue=new Select(wb);
+        dropdownvalue.selectByVisibleText(str);
+        return dropdownvalue;
+    }
+       
+    //To select the dropdown value using index method
+    public Select getDropdownValueByIndex(WebElement wb,int index)
+    {
+    	performElementClick(wb);
+    	Select dropdownvalue = new Select(wb);
+       	dropdownvalue.selectByIndex(index);
+    	return dropdownvalue;
+    }
+    
+    //To get the default selected value of the dropdown element
+    public String getDropdownFirstSelectedOption(WebElement wb)
+	 {
+        Select dropdownvalue = new Select(wb);
+	    WebElement option = dropdownvalue.getFirstSelectedOption();
+	    String  SelectedText = option.getText();
+		return SelectedText;
+	 }
+    
+    //To get all the options from the dropdownlist
+    public  List<String> getDropdownListOptions(WebElement wb)
+	 {
+      List<String> optionsList = new ArrayList<String>();
+      try
+      {
+      if(isElementDropDown(wb))
+      {
+      performElementClick(wb);
+	  Select se=new Select(wb);
+	  List <WebElement> elements = se.getOptions();
+      System.out.println("The States List is"+": " + elements.size());
+      int listSize = elements.size();
+  	
+      for(int i = 1; i < listSize ; i++) 
+       {
+         String optionsValue=elements.get(i).getText();
+         optionsList.add(optionsValue);
+       }
+      }
+      else 
+	  {
+	   System.out.println( "WebELement List list options is empty or doesnot exist in this dropdown.So could not fetch the text values " );
+	   logger.warn("WebELement List is empty or doesnot exist in this page.So could not fetch the text values " );
+	   }
+      }
+      
+     catch(Exception E)
+      {
+    	  System.out.println(wb +" is not a dropdown");
+      }
+      return optionsList;
+	 }
+}  
+	 
+
+    
+
+
