d3f9349134d0f1fe84b773e83e50649babd708ed
srivchan <srividhya.chandru@gmail.com>
2021-08-30 19:02:42 -0400

    HomePage Testcases

--- src/main/java/com/DollarDays/utilities/ConstantsUtils.java
+++ src/main/java/com/DollarDays/utilities/ConstantsUtils.java
@@ -6,7 +6,7 @@ public class ConstantsUtils
 	
 	public static final int DEFAULT_IMPLICIT_WAIT=20;
 	
-	public static final String BASEURL= "https://www.dollardays.com" ;
+	public static final String BASEURL= "https://www.dollardays.com/" ;
 	public static final String HOMEPAGETITLE ="DollarDays | Bulk Goods for Nonprofits, Businesses & Schools | Wholesale Products"; 
 	public static final String LOGINPAGETITLE="Login or Register - DollarDays";
 	public static final String MYPROFILETITLE="Profile - DollarDays";
@@ -35,6 +35,24 @@ public class ConstantsUtils
 	public static final String SIGNOUTURL ="https://www.dollardays.com/" ;
 	public static final String ACCOUNTTERMSURL ="https://www.dollardays.com/aboutus/policies.aspx#Return";
 	public static final String PRIVACYTERMSURL ="https://www.dollardays.com/aboutus/policies.aspx#Shipping";
+
+	public static final String NOWTRENDING = "PPE, Masks & Sanitizers:Back to School Store:Monthly Ad";
+	public static final String CATEGORIES = "Baby:Backpacks:Bedding, Bath & Decor:Clothing & Shoes:Electronics:Food:Hardware, Tools & Outdoors:Holiday & Party:Household Essentials:Kitchen & Dining:Luggage & Travel Accessories:Personal Care:Pet Supplies:PPE & Masks:School & Office:Toys & Games";
+	public static final String PROGRAMS =  "For Nonprofits,For Educators,For Retailers,For Emergency Preparedness";
+	public static final String HELPSETTINGS ="Your Account,Contact Us,FAQs,Return Policy,Shipping Policy";
+	public static final String SIGNINTEXT = "Sign in";
+	public static final String HEADERRIGHT = "Sign in,Wishlist,Cart";
+	public static final String SEARCHBARFIELD ="Search for items in bulk";
+	public static final String WISHLISTDROPDOWN = "Learn More,Create a Wishlist,Donate Today";
+	public static final String PHONENUMBERMESSAGE = "Call today!  (877) 837-9569";
+	public static final String CARTICONTOOLTIP = "Your Cart is Empty";
+	public static final String CUSTOMERSERVICEFOOTER ="Contact Us,FAQs,Privacy Policy,Return Policy,Shipping Policy,Terms & Conditions";
+	public static final String CUSTOMERSERVICEFOOTERTITLES ="Contact Us - DollarDays,FAQs - DollarDays,Privacy Policy - DollarDays,Policies - DollarDays,Shipping Policy - DollarDays,Policies - DollarDays";
+	public static final String SHOPBYCATEGORYTEXT ="Masks:Sanitizers, Wipes & Disinfectants:School & Office:Backpacks:Cars, Vehicles & RC Toys:Electronics:Halloween:Baby:Kid's Clothing:Kid's Shoes:Socks:Flip-Flops:Pet Supplies:Hygiene Kits:Bath:Blankets:Kitchen & Dining:Food";
+	public static final String SHOPBYCATEGORYTITLE ="Wholesale Disposable Masks in Bulk - DollarDays:Wholesale Sanitizers, Disinfectants, & Wipes - DollarDays:Wholesale School & Office Supplies – Bulk School Supplies - DollarDays:Wholesale Backpacks, Bulk Backpacks – DollarDays - DollarDays:Wholesale Toy Cars - Wholesale Diecast Cars - Bulk Trucks- Wholesale RC Toys - DollarDays";
+	public static final String COPYRIGHTINFO = "Copyright © 2001-2021\r\n"
+			+ "DollarDays\r\n"
+			+ "All rights reserved.";
 
 
 }
